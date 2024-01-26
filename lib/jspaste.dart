7197c763c8f17b47f768fcc204d46461f232b09c
Krak798 <krak798@outlook.com>
2024-01-26 19:12:59 +0100

    docs: changed standard library comment

--- lib/jspaste.dart
+++ lib/jspaste.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// Standard JSPaste library.
 library;
 
 export 'src/jspaste_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
